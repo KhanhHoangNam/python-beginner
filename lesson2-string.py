a14777222a88acc489b7ae9b128afc378ba09ffe
khanhhn <chienbinhbattu9xpro@gmail.com>
2020-07-10 16:24:07 +0700

    Initialize commit

---
+++ lesson2-string.py
@@ -0,0 +1,8 @@
+print("Hello World")
+print(r"Neu mot ngay \nnguoi ra di voi vang")
+anotherStr = "5"
+vnexpress = anotherStr*5
+print(vnexpress)
+print(anotherStr in vnexpress)
+print(anotherStr[0])
+#Testing application using python
