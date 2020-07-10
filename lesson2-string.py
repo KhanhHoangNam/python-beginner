685e7fc336552420adf112f2c89ece43f9ee6a15
khanhhn <chienbinhbattu9xpro@gmail.com>
2020-07-10 18:03:35 +0700

    updating code

--- lesson2-string.py
+++ lesson2-string.py
@@ -1,8 +1,13 @@
 print("Hello World")
 print(r"Neu mot ngay \nnguoi ra di voi vang")
-anotherStr = "5"
+anotherStr = "Hoang Nam Khanh "
 vnexpress = anotherStr*5
 print(vnexpress)
 print(anotherStr in vnexpress)
 print(anotherStr[0])
+#Cat chuoi tu trai sang phai
+otherStr = anotherStr[1:(len(anotherStr) - 2)]
+#Cat chuoi tu phai sang trai
+#Nothing
+print(otherStr)
 #Testing application using python
