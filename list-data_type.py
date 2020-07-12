b8af3e19b28ef8c02787c50368e4eab77e983fc8
khanhhn <chienbinhbattu9xpro@gmail.com>
2020-07-13 00:53:32 +0700

    update code

--- list-data_type.py
+++ list-data_type.py
@@ -4,7 +4,16 @@ print("List data type Lesson")
 # List có khả năng lưu trữ mọi đối tượng của python.
 # vào bao gồm cả chính nó.
 
-aList = [1,2,3, ["Hoang", "Nam", "Khanh"]]
+aList = [1,2,3, ["Hoang", "Nam", "Khanh", [10, 100]]]
+aList += ["Nothing", "Everthing"]
+aList *= 3
+aSubList = "Nothing"
 anotherList = [i for i in range(10)]
+multipleList = [[n, n*1, n*2] for n in range(0,3)]
+aStringList = list("Hoang Nam Khanh")
 print(aList)
 print(anotherList)
+print(multipleList)
+print(aStringList)
+print(aSubList in aList)
+print(aList[3][0])
