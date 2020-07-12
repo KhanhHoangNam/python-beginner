1eca855346661c94e1c4d22d43a9db075f964122
khanhhn <chienbinhbattu9xpro@gmail.com>
2020-07-12 23:55:24 +0700

    update code

--- list-data_type.py
+++ list-data_type.py
@@ -1,1 +1,10 @@
 print("List data type Lesson")
+# Một list được giới hạn bởi các cặp ngoặc vuông
+# Các phần tử của list được ngăn cách nhau bởi dấu phẩy(,)
+# List có khả năng lưu trữ mọi đối tượng của python.
+# vào bao gồm cả chính nó.
+
+aList = [1,2,3, ["Hoang", "Nam", "Khanh"]]
+anotherList = [i for i in range(10)]
+print(aList)
+print(anotherList)
