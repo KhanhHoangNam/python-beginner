f591f03f50b4ab315d9df65911b798f622e3a0de
khanhhn <chienbinhbattu9xpro@gmail.com>
2020-07-16 23:07:14 +0700

    update code

---
+++ tuple.py
@@ -0,0 +1,7 @@
+# được giới hạn bởi cặp dấu ngoặc tròn ()
+# Các phần tử tuple được phân cách nhau bởi dấu phẩy (,)
+# tuple có khả năng chứa mọi giá trị
+# tuple truy xuất nhanh hơn list
+# dung lượng chiếm dung lượng trong bộ nhớ nhỏ hơn list
+# bảo vệ dữ liệu không bị thay đổi
+# có thể dùng làm key của dictionary
