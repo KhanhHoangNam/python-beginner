eed8e2548c19bb80075013f1c4b2aa05febe8ac4
khanhhn <chienbinhbattu9xpro@gmail.com>
2020-07-19 23:22:27 +0700

    tuple data types

--- tuple.py
+++ tuple.py
@@ -5,3 +5,23 @@
 # dung lượng chiếm dung lượng trong bộ nhớ nhỏ hơn list
 # bảo vệ dữ liệu không bị thay đổi
 # có thể dùng làm key của dictionary
+
+tup = (1,3,2,9,6, ("Hoang", "Nam", "Khanh"))
+aTuple = tuple("Hoang Nam Khanh")
+anotherTuple = tuple(i for i in range(10) if i % 2 != 0)
+print(tup)
+print(aTuple)
+print(tuple(anotherTuple))
+print(aTuple + anotherTuple)
+
+# a functions example
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
+
+print(quicksort([3,6,8,0,11,100,1]))
