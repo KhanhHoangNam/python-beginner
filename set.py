44f285deb0df17ea36cce431338dd3a08d1b564e
khanhhn <chienbinhbattu9xpro@gmail.com>
2020-07-21 22:52:05 +0700

    set data type

---
+++ set.py
@@ -0,0 +1,7 @@
+# Một set được giới hạn bởi một dấu cặp ngoặc nhọn({}),
+# Tất cả những gì nằm trong đó đều là phần tử của set
+# Các phần tử của set được ngăn cách nhau bởi dấu phẩy(,)
+# Set không chứa nhiều hơn một phần tử trùng lặp 
+# Set chỉ có thể chứa các hashtable object 
+# nhưng chính nó không phải là một hashtable object 
+# do đó không thể chứa set trong một set
